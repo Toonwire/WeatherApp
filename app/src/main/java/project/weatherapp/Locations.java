1650f28f30e143b35ca06ebfa19e837277891746
Jesper <jsppetersen@hotmail.com>
2015-06-17 14:30:49 +0200

    Små ændringer i Settings.java og activity_settings.xml

--- app/src/main/java/project/weatherapp/Locations.java
+++ app/src/main/java/project/weatherapp/Locations.java
@@ -1,7 +1,5 @@
 package project.weatherapp;
 
-/**
- * Created by Jesper on 17/06/15.
- */
 public class Locations {
+
 }
--- app/src/main/java/project/weatherapp/Settings.java
+++ app/src/main/java/project/weatherapp/Settings.java
@@ -1,29 +1,22 @@
 package project.weatherapp;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceFragment;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
-import android.widget.ToggleButton;
-import android.view.Menu;
 import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
 
-/**
- * Created by Jesper on 16/06/15.
- */
 public class Settings extends MainActivity {
 
     private Switch rain;
     private Switch humidity;
     private RadioButton metric;
     private RadioButton imperial;
+    private Button manageLocations;
+    private RadioGroup group;
 
     @Override
     protected void onCreate(Bundle bundle) {
@@ -34,6 +27,7 @@ public class Settings extends MainActivity {
         humidity = (Switch) findViewById(R.id.humidity_switch);
         metric = (RadioButton) findViewById(R.id.metric_radio_button);
         imperial = (RadioButton) findViewById(R.id.imperial_radio_button);
+        manageLocations = (Button) findViewById(R.id.manage_location);
 
         rain.setChecked(true);
         humidity.setChecked(true);
@@ -41,59 +35,42 @@ public class Settings extends MainActivity {
         rain.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                MainActivity.setRain(isChecked);
+                //MainActivity.setRain(isChecked);
             }
         });
 
         humidity.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                MainActivity.setHumidity(isChecked);
+                //MainActivity.setHumidity(isChecked);
             }
         });
 
-        RadioGroup group = (RadioGroup) findViewById(R.id.unit_radio_group);
+        group = (RadioGroup) findViewById(R.id.unit_radio_group);
+        group.clearCheck();
 
         group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
-                //TODO Make setUnit
-                MainActivity.setUnit(isChecked);
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                // TODO set MainActivity unit to the checkedId
+                // MainActivity.setUnit(checkedId);
             }
         });
 
+        manageLocations.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                goToManageLocations();
+            }
+        });
     }
 
     public void onResume() {
 
     }
 
-    public void onUnitRadioButtonClicked(View view) {
-        boolean metricChecked = ((RadioButton) view).isChecked();
-        boolean imperialChecked = ((RadioButton) view).isChecked();
-
-        switch(view.getId()) {
-            case R.id.imperial_radio_button:
-                if(imperialChecked) {
-                    metricChecked = false;
-
-                }
-                break;
-            case R.id.metric_radio_button:
-                if(metricChecked) {
-                    imperialChecked = false;
-                }
-                break;
-        }
-    }
-
 
     public void goToManageLocations() {
         Intent intent = new Intent(Settings.this,Locations.class);
-        EditText edittext = (EditText) findViewById(R.id.manage_location);
-        String message = edittext.getText().toString();
-        intent.putExtra(null,message);
         startActivity(intent);
     }
 }
