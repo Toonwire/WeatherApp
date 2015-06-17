3e28b8325818fedc0134da9d68e9b021f8227e38
Toonwire <Toonwire@hotmail.com>
2015-06-17 13:51:36 +0200

    Dynamically added ImageView resources, based on JSON data

--- app/src/main/java/project/weatherapp/MainActivity.java
+++ app/src/main/java/project/weatherapp/MainActivity.java
@@ -2,36 +2,22 @@ package project.weatherapp;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.os.Bundle;
-import android.widget.TextView;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
-
-import java.text.DecimalFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.TimeZone;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.app.ListActivity;
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
-
-import org.json.JSONObject;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class MainActivity extends Activity {
 
@@ -58,6 +44,8 @@ public class MainActivity extends Activity {
     private static final String TAG_RAIN = "rain";
     private static final String TAG_3H = "3h";
 
+    private static final String TAG_DT = "dt";
+
     private static final String TAG_CITY = "name";
 
     private String id;
@@ -71,6 +59,7 @@ public class MainActivity extends Activity {
     private String windSpeed;
     private String windDeg;
     private String rain;
+    private String dt;
 
     private static String url;
 
@@ -83,8 +72,29 @@ public class MainActivity extends Activity {
     private TextView tvRain;
     private TextView tvCity;
 
+    private ImageView imTemperature;
+    private ImageView imPressure;
+    private ImageView imHumidity;
+    private ImageView imWindDirection;
+    private ImageView imSunrise;
+    private ImageView imSunset;
+    private ImageView imRain;
+    private ImageView imWeatherIcon;
+
     DecimalFormat df = new DecimalFormat("#.00");
     SimpleDateFormat sdf = new SimpleDateFormat("HH:mm"); // the format of your date
+
+    public enum UnitSystem{
+        METRIC, IMPERIAL
+    }
+
+    UnitSystem settingsUnitSystem = UnitSystem.METRIC;
+    boolean settingsRain = true;
+    boolean settingHumidity = true;
+    boolean settingPressure = true;
+    boolean settingsWind = true;
+    boolean settingsSunriseSet = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,8 +110,17 @@ public class MainActivity extends Activity {
         tvRain = (TextView) findViewById(R.id.data_view_rain);
         tvCity = (TextView) findViewById(R.id.city_name);
 
+        imTemperature = (ImageView) findViewById(R.id.data_view_temperature_icon);
+        imPressure = (ImageView) findViewById(R.id.data_view_pressure_icon);
+        imHumidity = (ImageView) findViewById(R.id.data_view_humidity_icon);
+        imWindDirection = (ImageView) findViewById(R.id.data_view_wind_direction_icon);
+        imSunrise = (ImageView) findViewById(R.id.data_view_sunrise_icon);
+        imSunset = (ImageView) findViewById(R.id.data_view_sunset_icon);
+        imRain = (ImageView) findViewById(R.id.data_view_rain_icon);
+        imWeatherIcon = (ImageView) findViewById(R.id.main_weather_image);
+
         // Set by location in smartphone
-        url = "http://api.openweathermap.org/data/2.5/weather?q=Helsingor,dk";
+        url = "http://api.openweathermap.org/data/2.5/weather?q=Aalborg,dk";
         sdf.setTimeZone(TimeZone.getTimeZone("GMT+1")); // give a timezone reference for formating (see comment at the bottom
 
         new GetWeatherToday().execute();
@@ -139,6 +158,9 @@ public class MainActivity extends Activity {
 
                     // Getting JSON Array node
 
+                    dt = jsonObj.getString(TAG_DT);
+                    city = jsonObj.getString(TAG_CITY);
+
                     JSONObject todaysSysResources = jsonObj.getJSONObject(TAG_SYS);
                     country = todaysSysResources.getString(TAG_COUNTRY);
                     sunrise = todaysSysResources.getString(TAG_SUNRISE);
@@ -160,7 +182,7 @@ public class MainActivity extends Activity {
                     JSONObject todaysRainResources = jsonObj.getJSONObject(TAG_RAIN);
                     rain = todaysRainResources.getString(TAG_3H);
 
-                    city = jsonObj.getString(TAG_CITY);
+
 
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -175,6 +197,7 @@ public class MainActivity extends Activity {
         @Override
         protected void onPostExecute(Void result) {
             super.onPostExecute(result);
+
             // Dismiss the progress dialog
             if (pDialog.isShowing())
                 pDialog.dismiss();
@@ -184,24 +207,167 @@ public class MainActivity extends Activity {
 
             long unixSeconds = Long.parseLong(sunrise);
             Date date = new Date(unixSeconds*1000L);
-            sunrise = sdf.format(date);
+            String sunriseTime = sdf.format(date);
 
             unixSeconds = Long.parseLong(sunset);
             date = new Date(unixSeconds*1000L);
-            sunset = sdf.format(date);
+            String sunsetTime = sdf.format(date);
 
+            Log.d("testingss", id);
             tvCity.setText(city);
-            tvSunrise.setText(sunrise);
-            tvSunset.setText(sunset);
+            tvSunrise.setText(sunriseTime);
+            tvSunset.setText(sunsetTime);
             tvWindSpeed.setText(df.format(Double.parseDouble(windSpeed)) + "m/s");
             if (rain != null)
                 tvRain.setText(df.format(Double.parseDouble(rain)) + "mm");
             tvTemperature.setText(df.format(Double.parseDouble(temperature) - 273.15) + "°C");
-            tvPressure.setText(pressure + " hPa");
+            tvPressure.setText(Integer.parseInt(pressure) + " hPa");
             tvHumidity.setText(humidity + "%");
 
+            imWindDirection.setRotation(Float.parseFloat(windDeg));
+
+            Bitmap mBitmap = null;
+
+            switch(id){
+                case "200":
+                case "201":
+                case "202":
+                case "210":
+                case "211":
+                case "212":
+                case "221":
+                case "230":
+                case "231":
+                case "232":
+
+                    mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.thunder);
+                    break;
+
+                case "300":
+                case "301":
+                case "302":
+                case "310":
+                case "311":
+                case "312":
+                case "321":
+                case "511":
+                case "520":
+                case "521":
+                case "522":
+
+                    mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.rain);
+                    break;
+
+                case "500":
+                case "501":
+                case "502":
+                case "503":
+                case "504":
+
+                    if (day())
+                        mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.day_rain);
+
+                    else
+                        mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.night_rain);
+
+                    break;
+
+                case "600":
+                case "601":
+                case "602":
+                case "611":
+                case "615":
+                case "616":
+                case "621":
+
+                    mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.snow);
+                    break;
+
+                case "701":
+                case "711":
+                case "721":
+                case "731":
+                case "741":
+
+                    mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.mist);
+                    break;
+
+                case "800":
+
+                    if (day())
+                       mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sun);
+
+                    else
+                       mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.moon);
+
+                    break;
+
+                case "801":
+
+                    if (day())
+                        mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.few_clouds_day);
+                    else
+                        mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.few_clouds_night);
+
+                    break;
+
+                case "802":
+                case "803":
+                case "804":
+
+                    if (day())
+                        mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.clouds);
+                    else
+                        mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.clouds);
+
+                    break;
+
+                case "900":
+                case "901":
+                case "902":
+                case "903":
+                case "904":
+                case "905":
+                case "906":
+                    mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.tornado);
+                    break;
+
+                default:
+                    mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.raindrop);
+
+            }
+
+            imWeatherIcon.setImageBitmap(mBitmap);
+
+        }
+        // checks if the current time is within the day time interval, created by sunrise and sunset
+        private boolean day(){
+            return Integer.parseInt(sunrise) < Integer.parseInt(dt) && Integer.parseInt(dt) < Integer.parseInt(sunset);
         }
 
     }
+
+    public void setUnitSystem(UnitSystem unitSystem){
+        settingsUnitSystem = unitSystem;
+    }
+
+    public void setRain(boolean checked){
+        settingsRain = true;
+    }
+
+    public void setHumidity(boolean checked){
+        settingsRain = true;
+    }
+
+    public void setPressure(boolean checked){
+        settingsRain = true;
+    }
+
+    public void setSunriseSet(boolean checked){
+        settingsRain = true;
+    }
+
+
+
 
 }
--- app/src/main/java/project/weatherapp/Settings.java
+++ app/src/main/java/project/weatherapp/Settings.java
@@ -11,6 +11,5 @@ public class Settings extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
-        addPreferencesFromResource(R.);
     }
 }
