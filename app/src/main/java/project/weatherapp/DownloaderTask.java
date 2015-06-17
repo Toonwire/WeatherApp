7b3fcd2cdcce0b06570dd11f323d633844e8f9f4
sebastiannyholm <sebn89@gmail.com>
2015-06-17 11:30:09 +0200

    JSON

---
+++ app/src/main/java/project/weatherapp/DownloaderTask.java
@@ -0,0 +1,13 @@
+package project.weatherapp;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by sebastiannyholm on 17/06/15.
+ */
+public class DownloaderTask extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+}
